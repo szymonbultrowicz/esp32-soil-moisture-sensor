de9f95104a5e79fac98365391e3225a2c6a12a3c
Szymon Bultrowicz <9554528+szymonbultrowicz@users.noreply.github.com>
2021-10-31 23:26:19 +0100

    Release script - publish to GH

--- release.py
+++ release.py
@@ -3,6 +3,8 @@ from functools import cmp_to_key
 import os
 import re
 import zipfile
+import subprocess
+
 import semver
 
 releases_dir = 'dist'
@@ -13,13 +15,15 @@ existing_release_files = glob(f'./{releases_dir}/v*.zip')
 if len(existing_release_files) > 0:
     all_versions = [re.search("(\d+\.\d+\.\d+)\.zip$", v).group(1) for v in existing_release_files]
     last_release = sorted(all_versions, key=cmp_to_key(semver.compare), reverse=True)[0]
-    print(last_release)
 new_version = semver.bump_patch(last_release)
+print(new_version)
 
-def zip_sources(version):
-    with zipfile.ZipFile(f'{releases_dir}\\v{version}.zip', 'w', zipfile.ZIP_DEFLATED) as zip_file:
+def zip_sources(out_file):
+    with zipfile.ZipFile(out_file, 'w', zipfile.ZIP_DEFLATED) as zip_file:
         for root, dirs, files in os.walk(sources_dir):
             for file in files:
                 zip_file.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), sources_dir))
 
-zip_sources(new_version)
+new_release_file = f'{releases_dir}/v{new_version}.zip'
+zip_sources(new_release_file)
+subprocess.run(['gh', 'release', 'create', f'v{new_version}', new_release_file, '-n', ''], check=True)
