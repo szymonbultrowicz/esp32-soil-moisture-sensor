1befc3bff82934547397e9771044e1c99b2e3c30
Szymon Bultrowicz <9554528+szymonbultrowicz@users.noreply.github.com>
2021-11-01 23:00:49 +0100

    Cleaned up dir structure

--- release.py
+++ release.py
@@ -18,13 +18,4 @@ tag_name = response.json()[0]['tag_name']
 latest_version = re.search('\d+\.\d+\.\d+$', tag_name).group(0)
 new_version = semver.bump_patch(latest_version)
 
-def zip_sources(out_file):
-    with zipfile.ZipFile(out_file, 'w', zipfile.ZIP_DEFLATED) as zip_file:
-        zip_file.write('main.py')
-        for root, dirs, files in os.walk(sources_dir):
-            for file in files:
-                zip_file.write(os.path.join(root, file))
-
-new_release_file = f'{release_dir}/v{new_version}.zip'
-zip_sources(new_release_file)
-subprocess.run(['gh', 'release', 'create', new_version, new_release_file, '-n', ''], check=True)
+subprocess.run(['gh', 'release', 'create', new_version, '-n', ''], check=True)
